a7d0f84ff9f7a5e08ed1f45199d770c5ac36b6f0
Sebastien Lemery <seblemery@gmail.com>
2016-01-27 18:14:36 -0500

    added a few things
    edited a few lines

--- scripts/eggdrop-pisg.tcl
+++ scripts/eggdrop-pisg.tcl
@@ -4,37 +4,47 @@
 # WARNING - READ THIS
 #
 # If you use this script, PLEASE read the documentation about the "Silent"
-# option. If you get the message "an error occured: Pisg v0.67 - perl irc
+# option. If you get the message "an error occured: Pisg v0.** - perl irc
 # statistics generator" in the channel, you are NOT running silent. Fix it.
 
-set pisgver "0.15"
+set pisgver "0.16"
 
 #Location of pisg execuitable perl script
-set pisgexe "/home/nf/pisg/pisg"
+set pisgexe "/home/bot/pisg-0.73/pisg"
 
 #URL of the generated stats
-set pisgurl "http://stats.nemesisforce.com/"
+set pisgurl "http://nemesisforce.com/ircstats"
 
 #channel that the stats are generated for
-set pisgchan "#nemesisforce"
+set pisgchan "#pisg"
 
-#Users with these flags can operate this function
-set pisgflags "nm"
+#Users with these flags can operate this function (friendly/friends by default)
+set pisgflags "f"
 
 #How often the stats will be updated in minutes, ie: 30 - stats will be updated every 30 minutes
-set pisgtime "30"
+set pisgtime "180"
 
-bind pub $pisgflags !stats pub:pisgcmd
+bind pub $pisgflags !pisgstats pub:pisgcmd1
 
-proc pub:pisgcmd {nick host hand chan arg} {
-	global pisgexe pisgurl pisgchan
-	append out "PRIVMSG $pisgchan :" ; if {[catch {exec $pisgexe} error]} { append out "$pisgexe an error occured: [string totitle $error]" } else { append out "Stats Updated: $pisgurl" }
+proc pub:pisgcmd1 {nick host hand chan arg} {
+	global pisgexe pisgurl
+	append out "PRIVMSG $chan :"  
+	if {[catch {exec $pisgexe} error]} { 
+	append out "$pisgexe an error occured: [string totitle $error]" 
+	} else { 
+	append out "Stats: [string tolower ${pisgurl}[string trimleft $chan # ].html]"
+	}
 	puthelp $out
 }
 
 proc pisgcmd_timer {} {
-	global pisgexe pisgurl pisgchan pisgtime
-	append out "PRIVMSG $pisgchan :" ; if {[catch {exec $pisgexe} error]} { append out "$pisgexe an error occured: [string totitle $error]" } else { append out "Stats Updated: $pisgurl" }
+	global pisgexe pisgurl pisgtime chan
+	append out "PRIVMSG gchan :" 
+	if {[catch {exec $pisgexe} error]} { 
+	append out "$pisgexe an error occured: [string totitle $error]" 
+	} else { 
+	append out "Stats: [string tolower ${pisgurl}[string trimleft $chan # ].html]"
+	}
 	puthelp $out
 	timer $pisgtime pisgcmd_timer
 }
